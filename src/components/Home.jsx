ae746d753e472b476286cd98c39c4f8a5eaace82
JayrajsinhHarichtech <shilpgroup47@gmail.com>
2025-12-08 12:51:05 +0530

    Image Changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -64,20 +64,26 @@ export default function Home() {
   }, []);
 
   useEffect(() => {
-    const targetDate = new Date("2025-12-13T00:00:00");
+    const targetDate = new Date("2025-12-13T10:00:00").getTime();
 
-    const timer = setInterval(() => {
-      const now = new Date();
+    const calculateTimeLeft = () => {
+      const now = new Date().getTime();
       const difference = targetDate - now;
 
-      const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
-      const minutes = Math.floor((difference / 1000 / 60) % 60);
-      const seconds = Math.floor((difference / 1000) % 60);
+      if (difference > 0) {
+        setTimeLeft({
+          days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+          hours: Math.floor(
+            (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+          ),
+          minutes: Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)),
+          seconds: Math.floor((difference % (1000 * 60)) / 1000),
+        });
+      }
+    };
 
-      setTimeLeft({ days, hours, minutes, seconds });
-    }, 1000);
-
+    calculateTimeLeft();
+    const timer = setInterval(calculateTimeLeft, 1000);
     return () => clearInterval(timer);
   }, []);
 
